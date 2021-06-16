836104d890f075a31d6551edaf3be4961ba1662c
Edinsson Melo <edinssonadrianmc@ufps.edu.co>
2021-06-16 17:54:27 -0500

    Se agrega el algoritmo hash MD5 para realizar el pago

--- src/app/components/inicio-mostrar-orden/inicio-mostrar-orden.component.ts
+++ src/app/components/inicio-mostrar-orden/inicio-mostrar-orden.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Md5 } from 'src/md5';
+import {Md5} from 'ts-md5/dist/md5';
 import Swal from 'sweetalert2';
 
 
@@ -14,14 +14,10 @@ import Swal from 'sweetalert2';
 export class InicioMostrarOrdenComponent implements OnInit {
   cliente : any = null
   orden : any = null
-<<<<<<< HEAD
-  signature: null
-  md5 = new Md5();
-=======
+  signature: any = null
   url ="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/";
 
 
->>>>>>> b4c6cbae6d8b573bd27a8d0da34b23bfd9dd0e34
   constructor(private router: Router ) { }
 
   ngOnInit(): void {
@@ -30,7 +26,9 @@ export class InicioMostrarOrdenComponent implements OnInit {
     
     this.cliente = JSON.parse(window.sessionStorage.getItem('cliente'));
     this.orden = JSON.parse(window.sessionStorage.getItem('orden'));
-    this.md5.appendStr('4Vj8eK4rloUd272L48hsrarnUA').appendStr('508029').appendStr(this.orden.id).appendStr(this.cliente.plan.precio+0.19*this.cliente.plan.precio).appendStr('COP').end();
+    console.log(this.orden.id);
+    console.log(this.cliente.plan.precio);
+    this.signature =Md5.hashStr('4Vj8eK4rloUd272L48hsrarnUA~'.concat('508029~').concat(this.orden.id).concat('~').concat(this.cliente.plan.precio).concat('~').concat('COP'));
     if (!this.orden) {
       Swal.fire(
         'El cliente no existe',
